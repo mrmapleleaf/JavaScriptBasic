7ccc3a203887522e70e65d4e78f65cf02effb6b8
mrmapleleaf <kaple.de.96@gmail.com>
2022-12-18 15:20:39 +0900

    learn primitive and object

--- Section4/main.js
+++ Section4/main.js
@@ -1,2 +1,27 @@
+//ストリクトモードに適応
+//'use strict';
 //グローバルオブジェクトを見る
 console.log(globalThis);
+
+//昔の変数宣言
+var hello = 'hello';
+console.log(hello);
+
+function func1() {
+  //このfunc1のみがストリクトモードになる
+  'use strict';
+  console.log('hello');
+}
+
+// grape = 'grape';
+// console.log(globalThis);
+
+const coffee = {
+  name: 'coffee latte',
+};
+
+const coffee2 = coffee;
+console.log(coffee === coffee2);
+
+coffee.name = 'espresso';
+console.log(coffee2.name);
